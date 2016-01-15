1389bd76f6c906064c933ed99c6ad310816753e9
Zack Winchell <zackwinchell@gmail.com>
2016-01-14 20:57:29 -0800

    cards are now appended to the DOM

--- main.js
+++ main.js
@@ -1,45 +1,91 @@
 $(document).ready(function(){
-  var cards = {deck: [], playerHand: [], dealerHand:[]};
-  var preload = {cardBacks: [new Image().src = "cards/cardBack.png"]};
+  var cards = {deck: [], player: [], dealer:[]};
+  var preload = {images: []};
+  var hideFun = {};
+
+  var $dealerHand = $('#dealerHand');
+  var $playerHand = $('#playerHand');
 
   for(var i = 1; i <= 13; i++){
-    preload.cardBacks.push(new Image().src = "cards/"+i+"clubs.png");
-    preload.cardBacks.push(new Image().src = "cards/"+i+"diamonds.png");
-    preload.cardBacks.push(new Image().src = "cards/"+i+"hearts.png");
-    preload.cardBacks.push(new Image().src = "cards/"+i+"spades.png");
+    preload.images.push(new Image().src = "cards/"+i+"clubs.png");
+    preload.images.push(new Image().src = "cards/"+i+"diamonds.png");
+    preload.images.push(new Image().src = "cards/"+i+"hearts.png");
+    preload.images.push(new Image().src = "cards/"+i+"spades.png");
   }
+  preload.images.push(new Image().src = "cards/cardBack.png");
 
   cards.draw = function(){
-    return Number(cards.deck.splice(_.random(0, cards.deck.length-1), 1));
-  }; //
+    return _.flatten(cards.deck.splice(_.random(0, cards.deck.length-1), 1));
+  };
 
-  newDeck = function(){
+  hideFun.newDeck = function(){
     cards.deck = [];
     for(var i = 1; i < 14; i++){
-      cards.deck.push(i, i, i, i);
+      cards.deck.push([i, 1]);
+      cards.deck.push([i, 2]);
+      cards.deck.push([i, 3]);
+      cards.deck.push([i, 4]);
     }
   }
 
-  clearHands = function(){
-    cards.dealerHand = [];
-    cards.playerHand = [];
+  hideFun.clearHands = function(){
+    cards.dealer = [];
+    cards.player = [];
   }
 
-  hit = function(who, count){
+  hideFun.clearTable = function(){
+    $('.gameArea .card').remove();
+  }
+
+  hideFun.popTable = function(){
+    var dealerCards = [];
+    cards.dealer.forEach(function(card, index){
+      console.log("card: ", _.flatten(card));
+      debugger;
+      if(index === 0){
+        var cardImg = preload.images[52];
+      }
+      else{
+        var cardImg = preload.images[4*(card[0]-1)+(card[1]-1)]; //math is awesome
+      }
+      var $card = $('<img>').attr('src', cardImg).addClass('card');
+      dealerCards.push($card);
+    });
+    $dealerHand.append(dealerCards);
+
+    var playerCards = [];
+    cards.player.forEach(function(card){
+      console.log("card: ", _.flatten(card));
+      var cardImg = preload.images[4*(card[0]-1)+(card[1]-1)]; //math is awesome
+      var $card = $('<img>').attr('src', cardImg).addClass('card');
+      playerCards.push($card);
+    });
+    $playerHand.append(playerCards);
+  }
+
+  hideFun.hit = function(who, count){
     count = typeof count !== 'undefined' ?  count : 1;
+    if(count > cards.deck.length){
+      hideFun.hit(who, (count-cards.deck.length)); //If there are not enough cards in the deck, draw the cards then shuffle to draw the rest
+    }
     for(var i = 0; i < count; i++){
-      cards[who+"Hand"].push(cards.draw());
+      cards[who].push(cards.draw());
     }
   }
 
   newGame = function(){
-    newDeck();
-    clearHands();
-    hit("dealer", 2);
-    hit("player", 2);
+    hideFun.newDeck();
+    hideFun.clearHands();
+    hideFun.clearTable();
+    hideFun.hit("dealer", 2);
+    hideFun.hit("player", 2);
+    hideFun.popTable();
   }
 
+
   newGame();
+
+  $('#newGame').click(newGame);
 
   debugger;
 
