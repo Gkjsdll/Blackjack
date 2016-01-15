f5ce8c28f3a5adc5bf69ea05299bb99d21e8484f
Zack Winchell <zackwinchell@gmail.com>
2016-01-14 18:34:08 -0800

    renamed aces & face cards to allow for preloading with a loop, preloaded card images

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
   var cards = {stored: {deck: [], playerHand: [], dealerHand:[]}};
+  var preload = {cardBacks: [new Image().src = "cards/cardBack.png"]};
+
+  for(var i = 1; i <= 13; i++){
+    preload.cardBacks.push(new Image().src = "cards/"+i+"clubs.png");
+    preload.cardBacks.push(new Image().src = "cards/"+i+"diamonds.png");
+    preload.cardBacks.push(new Image().src = "cards/"+i+"hearts.png");
+    preload.cardBacks.push(new Image().src = "cards/"+i+"spades.png");
+  }
 
   cards.draw = function(){
     return cards.stored.deck.splice(_.random(0, cards.stored.deck.length-1), 1);
